68d13482f34058a05156f16ee10e6d6733eb4bec
Yoon-kyungLee <yk2588@nate.com>
2024-01-29 22:17:49 +0900

    chore: table components에 mixin 적용

--- components/table/editDashboardTable/editDashboardTable.tsx
+++ components/table/editDashboardTable/editDashboardTable.tsx
@@ -4,21 +4,21 @@ import EditButton from "@/components/button/editButton/EditButton";
 
 function EditDashboardTable() {
   return (
-    <form className={clsx(styles["table-form"])}>
-      <div className={clsx(styles["dashboard-title"])}>
-        <div className={clsx(styles["dashboard-title-text"])}>
+    <form className={clsx(styles.tableForm)}>
+      <div className={clsx(styles.dashboardTitle)}>
+        <div className={clsx(styles.dashboardTitleText)}>
           {"dashboard title"}
         </div>
         <div>(color chips)</div>
       </div>
-      <div className={clsx(styles["dashboard-input-box"])}>
-        <label className={clsx(styles["label"])}>대시보드 이름</label>
+      <div className={clsx(styles.dashboardInputBox)}>
+        <label className={clsx(styles.label)}>대시보드 이름</label>
         <input
-          className={clsx(styles["dashboard-input"])}
+          className={clsx(styles.dashboardInput)}
           placeholder="뉴 프로젝트"
         />
       </div>
-      <div className={clsx(styles["edit-button"])}>
+      <div className={clsx(styles.editButton)}>
         <EditButton />
       </div>
     </form>
