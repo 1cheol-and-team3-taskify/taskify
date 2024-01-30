b92be92e068799d0d79c6c0371d5e1428304372c
CheeseB <2489ckckck@naver.com>
2024-01-30 21:34:05 +0900

    feat: 대시보드 카드 컴포넌트 작업중

--- components/card/DashboardCard.tsx
+++ components/card/DashboardCard.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import styles from "./DashboardCard.module.scss";
 
+const TEMP_IMAGE_PATH = "/temp/tempCardImage.png";
+
 const DashboardCard = () => {
-  return <div className={styles.container}>DashboardCard</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardImage}>
+        <Image src={TEMP_IMAGE_PATH} fill alt="Card Image" />
+      </div>
+      <div className={styles.cardInfo}>{"blabla.."}</div>
+    </div>
+  );
 };
 
 export default DashboardCard;
