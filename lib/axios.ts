66ab27db9706602ee460020fa8eb8392ab195fa6
CheeseB <2489ckckck@naver.com>
2024-02-02 22:57:00 +0900

    refactor: axios 테스트 코드 제거

--- lib/axios.ts
+++ lib/axios.ts
@@ -6,12 +6,7 @@ const authInstance = axios.create({
 
 authInstance.interceptors.request.use(config => {
   const accessToken = localStorage.getItem("login");
-
   if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
-
-  // test
-  // if (accessToken)
-  //   config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzA4LCJ0ZWFtSWQiOiIyLTMiLCJpYXQiOjE3MDY2OTM4OTIsImlzcyI6InNwLXRhc2tpZnkifQ.K6Sscplbko0_PPazWg0wFYna77L8vg-NvJJ9JJutucM`;
 
   return config;
 });
