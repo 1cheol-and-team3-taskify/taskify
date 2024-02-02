b7c274733e6f3ec2ab036f62e1ec45aa386ba34b
CheeseB <2489ckckck@naver.com>
2024-02-02 22:53:24 +0900

    feat: 로그인 기능 구현

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,14 +5,13 @@ const authInstance = axios.create({
 });
 
 authInstance.interceptors.request.use(config => {
-  const accessToken = window.localStorage.getItem("login");
+  const accessToken = localStorage.getItem("login");
 
-  // if (accessToken)
-  //   config.headers.Authorization = `Bearer ${dashboardId}`;
+  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
 
   // test
-  if (accessToken)
-    config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzA4LCJ0ZWFtSWQiOiIyLTMiLCJpYXQiOjE3MDY2OTM4OTIsImlzcyI6InNwLXRhc2tpZnkifQ.K6Sscplbko0_PPazWg0wFYna77L8vg-NvJJ9JJutucM`;
+  // if (accessToken)
+  //   config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzA4LCJ0ZWFtSWQiOiIyLTMiLCJpYXQiOjE3MDY2OTM4OTIsImlzcyI6InNwLXRhc2tpZnkifQ.K6Sscplbko0_PPazWg0wFYna77L8vg-NvJJ9JJutucM`;
 
   return config;
 });
