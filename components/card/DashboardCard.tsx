d2c9033b59d3f0c5f15a2fa5d49d65f54d29ffbf
JunCastle <juncastle97@gmail.com>
2024-02-08 18:51:36 +0900

    Fix: 할일 수정 모달 오류 수정

--- components/card/DashboardCard.tsx
+++ components/card/DashboardCard.tsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import styles from "./DashboardCard.module.scss";
 import TagChips from "@/components/chips/TagChips";
 import { generateRandomColorHexCode } from "@/utils/color";
 import clsx from "clsx";
 import Image from "next/image";
 import { CardPropsType } from "@/types/cards";
+import CardModal from "../modal/cardModal";
 
 const CALENDAR_ICON_PATH = "/icons/calendar.svg";
 
-const DashboardCard = ({ cardProps }: { cardProps: CardPropsType }) => {
+const DashboardCard = ({
+  cardProps,
+  onClick,
+}: {
+  cardProps: CardPropsType;
+  onClick: () => void;
+}) => {
+  const [isCardOpen, setIsCardOpen] = useState<boolean>(false);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={onClick}>
       {cardProps.imageUrl && (
         <img
           className={styles.cardImage}
--- components/modal/todoEditModal/TodoEditModal.tsx
+++ components/modal/todoEditModal/TodoEditModal.tsx
@@ -19,11 +19,8 @@ import { generateRandomColorHexCode } from "@/utils/color";
 import Plus from "@/components/button/plusBtn/PlusBtn";
 import AddImage from "@/components/mypage/AddImage";
 import { TodoEditType } from "@/types/cards";
-import axios from "@/lib/axios";
-import { title } from "process";
-import authInstance from "@/lib/axios";
-import { getMemberList } from "@/api/members";
-import InputDropdown from "@/components/inputdropdown/InputDropdown";
+import { getCardinfoList } from "@/api/cards";
+
 interface TodoEditModalProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
   onSelectItem: (selectedItemId: number) => void;
@@ -42,55 +39,83 @@ function TodoEditModal({ setIsOpen, onSelectItem }: TodoEditModalProps) {
     assignee: [],
     imageUrl: "",
   });
-  const [todoData, setTodoData] = useState<{ id: number; title: string }[]>([]);
+  // const [todoData, setTodoData] = useState<{ id: number; title: string }[]>([]);
 
-  useEffect(() => {
-    axios
-      .get("columns?dashboardId=3341")
-      .then(response => {
-        setTodoData(response.data.data);
-      })
-      .catch(error => {
-        console.error("데이터를 불러오는데 실패했습니다.", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("columns?dashboardId=3341")
+  //     .then(response => {
+  //       setTodoData(response.data.data);
+  //     })
+  //     .catch(error => {
+  //       console.error("데이터를 불러오는데 실패했습니다.", error);
+  //     });
+  // }, []);
 
-  const todoDropdownItems: DropdownItem[] = todoData.map(({ id, title }) => ({
-    id,
-    title,
-  }));
+  // const todoDropdownItems: DropdownItem[] = todoData.map(({ id, title }) => ({
+  //   id,
+  //   title,
+  // }));
 
-  const MemberListData = async () => {
-    try {
-      const dashMember = await getMemberList(dashboardId, 1, 20);
-      const formattedMembers = dashMember.members.map((member: any) => ({
-        profileImageUrl: member.profileImageUrl,
-        nickname: member.nickname,
-        id: member.id,
-      }));
-      setFormState(prevState => ({
-        ...prevState,
-        assignee: formattedMembers,
-      }));
-    } catch (error) {
-      console.error("GET 요청 실패 :", error);
-    }
-  };
-  useEffect(() => {
-    if (router.query.id) {
-      MemberListData();
-    }
-  }, [dashboardId]);
+  // const MemberListData = async () => {
+  //   try {
+  //     const dashMember = await getMemberList(dashboardId, 1, 20);
+  //     const formattedMembers = dashMember.members.map((member: any) => ({
+  //       profileImageUrl: member.profileImageUrl,
+  //       nickname: member.nickname,
+  //       id: member.id,
+  //     }));
+  //     setFormState(prevState => ({
+  //       ...prevState,
+  //       assignee: formattedMembers,
+  //     }));
+  //   } catch (error) {
+  //     console.error("GET 요청 실패 :", error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   if (router.query.id) {
+  //     MemberListData();
+  //   }
+  // }, [dashboardId]);
 
-  const handleSelectedId = (selectedItemId: number) => {
-    setFormState(prevState => ({
-      ...prevState,
-      assigneeUserId: selectedItemId,
-    }));
-  };
+  // const handleSelectedId = (selectedItemId: number) => {
+  //   setFormState(prevState => ({
+  //     ...prevState,
+  //     assigneeUserId: selectedItemId,
+  //   }));
+  // };
   const handleTodoEditClick = async (event?: FormEvent) => {
     if (event) event.preventDefault();
   };
+
+  const CardListData = async (cardId: number) => {
+    try {
+      await getCardinfoList(cardId);
+      console.log("Success");
+    } catch (error) {
+      console.log("GET 요청 실패: ", error);
+    }
+  };
+
+
+    // useEffect(() => {
+    //   CardListData(cardId);
+    // }, [cardId]);
+
+    // const [tagInput, setTagInput] = useState("");
+    // const [tags, setTags] = useState([]);
+
+    // const handleTagInputChange = e => {
+    //   setTagInput(e.target.value);
+    // };
+
+    // const handleTagInputKeyDown = e => {
+    //   if (e.key === "Enter" && tagInput.trim() !== "") {
+    //     setTags(prevTags => [...prevTags, tagInput.trim()]);
+    //     setTagInput("");
+    //   }
+    // };
 
   return (
     <ModalPortal>
